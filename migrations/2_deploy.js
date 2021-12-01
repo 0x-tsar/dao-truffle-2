ef27da7efc52fa9085fe1751f038d73f08bd9b5f
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-12-01 11:28:46 -0300

    :sparkles:

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,8 +3,8 @@ const Token = artifacts.require("Token");
 
 module.exports = async (deployer) => {
   await deployer.deploy(Token);
-  const token = Token.deployed();
+  const token = await Token.deployed();
 
   await deployer.deploy(DAO, token.addres);
-  const dao = DAO.deployed();
+  const dao = await DAO.deployed();
 };
